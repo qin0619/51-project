7926d3ea185e859a54dfda58d6bc4cf42c1c6b52
qin0619 <estsrfs0619@gmail.com>
2024-10-18 00:12:25 +0800

    LCD时钟

--- 10-1DS1302时钟/src/DS1302.h
+++ 10-1DS1302时钟/src/DS1302.h
@@ -1,3 +1,11 @@
 #ifndef __DS1302_H__
 #define __DS1302_H__
+extern unsigned char DS1302_Time[];
+void DS1302_Init(void);
+void DS1302_WriteByte(unsigned char Command,Data);
+unsigned char DS1302_ReadByte(unsigned char Command);
+void DS1302_ReadTime(void);
+void DS1302_SetTime(void);
+unsigned char DS1302_DToB(unsigned char Data);
+unsigned char DS1302_BToD(unsigned char Data);
 #endif
--- 10-1DS1302时钟/src/main.c
+++ 10-1DS1302时钟/src/main.c
@@ -1,10 +1,24 @@
 #include "REGX52.H"
 #include "LCD1602.h"
 #include "DS1302.h"
+#include "Delay.h"
+
 void main(){
     LCD_Init();
-    LCD_ShowString(1,1,"RTC");
+    DS1302_Init();
+    DS1302_SetTime();
+    LCD_ShowString(1,3,":");
+    LCD_ShowString(1,6,":");
+    LCD_ShowString(2,3,":");
+    LCD_ShowString(2,6,":");
     while(1){
-
+       DS1302_ReadTime();
+       LCD_ShowNum(1,1,DS1302_Time[0],2);
+       LCD_ShowString(1,3,":");
+       LCD_ShowNum(1,4,DS1302_Time[1],2);
+       LCD_ShowNum(1,7,DS1302_Time[2],2);
+       LCD_ShowNum(2,1,DS1302_Time[3],2);
+       LCD_ShowNum(2,4,DS1302_Time[4],2);
+       LCD_ShowNum(2,7,DS1302_Time[5],2);
     }
     }
